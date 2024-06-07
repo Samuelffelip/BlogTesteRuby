5f5051d86efe6766b9b38c0f67ccdf1ccd915993
Samuel Felipe <samuelzfelipe01@gmail.com>
2024-06-07 19:35:34 -0300

    primeiro commit

---
+++ db/migrate/20240607211734_create_posts.rb
@@ -0,0 +1,15 @@
+class CreatePosts < ActiveRecord::Migration[7.1]
+  def change
+    create_table :posts do |t|
+      t.string :name
+      t.string :title
+      t.text :content
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :posts
+  end
+end
